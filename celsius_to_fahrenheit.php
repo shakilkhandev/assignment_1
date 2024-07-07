9bd511933695cdde307c54366e2d08e87965ece6
shakilkhan2045 <shakilkhanfv@gmail.com>
2024-07-08 05:41:17 +0600

    formula of fahrenheit corrected  now everything is alright

--- celsius_to_fahrenheit.php
+++ celsius_to_fahrenheit.php
@@ -2,7 +2,7 @@
 
 $celsius = 32 ;                //variable to store
 
-$fahrenheit = 9/5 * $celsius ; //formula to calculate temp celsius to fahrenheit 
+$fahrenheit = 9/5 * $celsius +32 ; //formula to calculate temp celsius to fahrenheit 
 
 printf (" $celsius Degree Celsius =  %.2f  ", $fahrenheit ) ;  //printing value with 2 digit decimal point 
 
